873d056a4fa05ddeb73f923d39409c10ede8eabc
Joshy Orndorff <admin@joshyorndorff.com>
2021-04-19 08:44:44 -0400

    Major breakthrough after learning about #[wasm_bindgen(skip)]

--- examples/add/index.js
+++ examples/add/index.js
@@ -2,5 +2,22 @@
 // example
 const rust = import('./pkg');
 rust
-  .then(m => alert('1 + 2 = ' + m.add(1, 2)))
+  .then(({add, Machine}) => {
+
+    console.log('1 + 2 = ' + add(1, 2));
+    console.log("Joshy was here");
+    // console.log(greet("Joshy"));
+
+    let thing = new Machine();
+    console.log("Machine Created");
+    console.log(thing.toJSON());
+
+    for( let i = 0; i < 10; i++ ) {
+      // console.log(`Loop iteration ${i}`);
+      thing.inc_a();
+      console.log(thing.a);
+    }
+
+    console.log(`The inner storage item is ${thing.get_inner_storage_item()}`)
+  })
   .catch(console.error);
--- examples/add/src/lib.rs
+++ examples/add/src/lib.rs
@@ -1,6 +1,69 @@
 use wasm_bindgen::prelude::*;
 
+// Allows us to use Javascript's alert directly from Rust.
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
 #[wasm_bindgen]
 pub fn add(a: u32, b: u32) -> u32 {
     a + b
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+// #[wasm_bindgen]
+// pub fn get_thing() -> Thing {
+//     Thing {
+//         a: 1,
+//         b: 2,
+//     }
+// }
+
+#[wasm_bindgen(inspectable)]
+pub struct Machine {
+    /// Example field A
+    pub a: u32,
+    /// Example field B
+    pub b: u32,
+    // WARNING: These macros have to go before the doc comments, not after
+    #[wasm_bindgen(skip)]
+    /// Example field that does not impl the wasm bindgen traits. I'm hoping that having the 
+    /// skip here will allow us to still use this type in javascript.
+    pub inner: NotWasmFriendlyRuntime,
+}
+
+/// A struct that is not ready to cross the wasm boundary.
+/// Notice the missing #[wasm_bindgen]. This is
+/// analogue to FRAME's Runtime, Call, etc types.
+pub struct NotWasmFriendlyRuntime {
+    pub storage_item: u64,
+}
+
+#[wasm_bindgen]
+impl Machine {
+
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self {
+            a: 1,
+            b: 2,
+            inner: NotWasmFriendlyRuntime {
+                storage_item: 4,
+            }
+        }
+    }
+
+    /// A method that can mutate this object and be called from javascript
+    pub fn inc_a(&mut self) {
+        self.a += 1;
+    }
+
+    pub fn get_inner_storage_item(&self) -> u64 {
+        self.inner.storage_item
+    }
 }
